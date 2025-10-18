3174428e00722a097ed758d919da23d45be4a29a
BalajiMogarge <balajimogarge94@gmail.com>
2025-10-19 02:56:06 +0530

    Add Basic Math - Reverce Number

--- DSA/BasicMath.cpp
+++ DSA/BasicMath.cpp
@@ -9,7 +9,19 @@ int DigitCount(int num){
     }
     return count;
 }
+int RevercedNum(int num){
+    int rnum = 0;
+    int rem = 0;
+    while(num>0){
+        rem = num%10;
+        rnum += rem;
+        rnum *= 10;
+        num/=10;
+    }
+    return (rnum/10);
+}
 int main(){
     cout << "Number of Digits are: " << DigitCount(12345) << endl;
+    cout << "Reverce of Number is: " << RevercedNum(12345) << endl;
     return 0;
 }
