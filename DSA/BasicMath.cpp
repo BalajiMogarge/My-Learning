f1c5b8baf9f34edb4fc1c9851b1f9dab3a8c38a0
BalajiMogarge <balajimogarge94@gmail.com>
2025-10-19 03:23:55 +0530

    Add Basic Math - Checking Armstrong Number

--- DSA/BasicMath.cpp
+++ DSA/BasicMath.cpp
@@ -14,20 +14,36 @@ int RevercedNum(int num){
     int rem = 0;
     while(num>0){
         rem = num%10;
-        rnum += rem;
-        rnum *= 10;
+        rnum = rnum * 10 + rem;
         num/=10;
     }
-    return (rnum/10);
+    return (rnum);
 }
-int CheckPalindrom(int num){
+bool CheckPalindrom(int num){
     if(num == RevercedNum(num))return true;
     else return false;
+}
+bool CheckArmstrong(int n){
+    int sum = 0;
+    int num = n;
+    while (n>0){
+        int rem = n%10;
+        n /= 10;
+        sum += rem*rem*rem;
+    }
+    if(num==sum){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 int main(){
     cout << "Number of Digits are: " << DigitCount(12345) << endl;
     cout << "Reverce of Number is: " << RevercedNum(12345) << endl;
     cout << "Is 121 a Palindrome: ";
     CheckPalindrom(121)?cout<<"Yes\n":cout<<"No\n";
+    cout << "Is 153 a Armstrong: ";
+    CheckArmstrong(153)?cout<<"Yes\n":cout<<"No\n";
     return 0;
 }
